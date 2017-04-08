110a40bb66a6750316bfd426f63d104ce2a87c2f
Bien Justin Mandac <justinmandaca@gmail.com>
2017-04-08 11:55:35 +0800

    added header-logo styles

--- src/app/header-logo.js
+++ src/app/header-logo.js
@@ -1,18 +1,26 @@
 import React, {Component, render} from 'react'
 
 export default class HeaderLogo extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             width: 24,
-            height: 24
+            height: 24,
+            type: ''
         }
     }
 
-    render() {
+    render(props) {
+        console.log(this.props)
+        const width = this.props.width || this.state.width 
+        const height = this.props.height || this.state.height
         const styles = {
-            width: `${this.state.width}px`,
-            height: `${this.state.height}px`,
+            width: `${width}px`,
+            height: `${height}px`, 
+            paddingTop : `8px`,
+            paddingBottom: `8px`,
+            paddingLeft: `8px`,
+            paddingRight: `8px`
         }
 
         return <div style={styles} className="header-logo vagary-logo">
